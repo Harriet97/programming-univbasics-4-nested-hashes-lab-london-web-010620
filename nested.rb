fa8ff0a4f0dfb1b36553709310a12b71e245634b
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-09 14:53:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -77,14 +77,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        },
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
         }
      }
 
-programmer_hash
+programmer_hash[:yukihiro_matsumoto = {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]}]
     
 end
 
