86b4d48aa61e7cef78d4c2b2d81f1869ea939e7e
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-09 15:08:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,8 +126,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     dennis_new_language = "Assembly"
-  programmer_hash[:dennis_ritchie][:languages]= dennis_new_language
+     
+  programmer_hash[:dennis_ritchie][:languages]= programmer_hash[:dennis_ritchie][:languages]+ "Assembly"
   programmer_hash[:dennis_ritchie][:languages]
 
 end
